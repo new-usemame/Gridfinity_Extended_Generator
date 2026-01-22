1b37f01c688d46106b37383401812a4fc8de186c
anonymous <anonymous@users.noreply.github.com>
2026-01-22 02:21:04 -0500

    Update openscad service

--- backend/src/services/openscad.ts
+++ backend/src/services/openscad.ts
@@ -382,7 +382,7 @@ module segment_base(width_units, depth_units, left_edge, right_edge, front_edge,
             if (left_edge == "male") {
                 if (depth_units > 1) {
                     for (i = [1 : max(1, depth_units) - 1]) {
-                    console.log("[NOT CORNER]")
+                    console.log("[NOT CORNER]");
                         color([0, 1, 0]) translate([grid_offset_x, grid_offset_y + i * grid_unit, 0])
                         rotate([0, 0, -90])
                         male_tooth_3d(edge_pattern, plate_height);
